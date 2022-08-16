5e6b6d090c1bb738617910fadb400605ee730e24
Thatcher Deyoe <thatcher.deyoe@troyweb.com>
2022-08-16 16:46:53 -0400

    Adding tailwind

---
+++ resources/js/components/user/UserDashboard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="header-content">
+            <user-nav></user-nav>
+    </div>
+
+</template>
+<script>
+
+import UserNav from '/resources/js/components/user/UserNav.vue'
+export default {
+    mounted() {
+        console.log('dashboard mounted');
+    },
+    components: {
+        UserNav
+    }
+}
+</script>
